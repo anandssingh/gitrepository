33332664ea977aca4ad106e98545e48730866ff3
Anand <Anand@DESKTOP-17O0CU8>
2020-09-26 20:43:23 +0530

    changes in runner

--- DataStructure/src/BinaryTree/runner.java
+++ DataStructure/src/BinaryTree/runner.java
@@ -9,6 +9,8 @@ public class runner {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
+		
+		System.out.println("hello world");
 	}
 
 }
