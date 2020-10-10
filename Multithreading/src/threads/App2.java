82caa149c6ca699c68c2e3ab4ac966c73c027cbd
Anand <Anand@DESKTOP-17O0CU8>
2020-10-10 20:09:29 +0530

    threads

---
+++ Multithreading/src/threads/App2.java
@@ -0,0 +1,17 @@
+package threads;
+
+
+public class App2 {
+	
+
+	public static void main(String[] args) {
+		
+	String Name = Thread.currentThread().getName();
+		
+		
+		System.out.println("Thread " + Name );
+		
+		
+	}
+
+}
