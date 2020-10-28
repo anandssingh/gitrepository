46a7cc705dbbfe0264b7b479cb4bc51d05a3c1fb
Anand <Anand@DESKTOP-17O0CU8>
2020-10-29 00:32:48 +0530

    1st commit

---
+++ CoreJavaExamples/src/Examples/oneChileStaticBlock.java
@@ -0,0 +1,35 @@
+package Examples;
+
+public class oneChileStaticBlock extends twoparent {
+	static {
+		System.out.println("inside child satic block");
+	}
+
+	oneChileStaticBlock() {
+		System.out.println("inside child constructor ");
+	}
+
+	{
+		System.out.println("inside child initialization block");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("inside main");
+		new oneChileStaticBlock();
+		// new oneChileStaticBlock();
+		System.out.println("last main");
+	}
+}
+
+class twoparent {
+	static {
+		System.out.println("inside parent Static block");
+	}
+	{
+		System.out.println("inside parent initialisation block");
+	}
+
+	twoparent() {
+		System.out.println("inside parent constructor");
+	}
+}
