3d5314665770b245c98ef7224a440df9cd235a3f
Anand <Anand@DESKTOP-17O0CU8>
2020-10-29 00:34:27 +0530

    1st commit

---
+++ mycoolapp/src/test/java/com/lov2code/mycoolapp/AppTest.java
@@ -0,0 +1,20 @@
+package com.lov2code.mycoolapp;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
